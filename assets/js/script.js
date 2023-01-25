6216f779af6a685243979e3b1a5bdfbdfb37975d
Boualem <myboualem@gmail.com>
2023-01-25 22:28:51 +0100

    améliorations globales

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,31 @@
 function calculate() {
-    let num1 = document.getElementById("number1").value.replace(/\s/g,'');
-    let num2 = document.getElementById("number2").value.replace(/\s/g,'');
-    let result = document.getElementById("result");
-    if (isNaN(num1) || isNaN(num2) || num2==0) {
-        result.innerHTML = "Entrez des nombres valides";
-    } else {
-        var calcul = (num1 * 1000000) / num2;
-        result.innerHTML = calcul.toLocaleString();
+    let totalError = 0;
+    let totalOperation = 0;
+    for (let i = 1; i <= 7; i++) {
+      let errorValue = document.getElementById("error" + i).value.replace(/\s/g,'');
+      let operationValue = document.getElementById("operation" + i).value.replace(/\s/g,'');
+      if (errorValue !== "") {
+        totalError += parseInt(errorValue);
+      }
+      if (operationValue !== "") {
+        totalOperation += parseInt(operationValue);
+      }
     }
-}
+    if (isNaN(totalError) || isNaN(totalOperation) || totalOperation==0) {
+          document.getElementById("final-result").innerHTML = "Entrez des nombres valides";
+      } else {
+    let result = (totalError*1000000)/totalOperation;
+    document.getElementById("final-result").innerHTML = result.toLocaleString("fr-FR", { minimumFractionDigits: 0 });
+      }
+  };
+
+  function copyResult() {
+    let result = document.getElementById("final-result");
+    let tempInput = document.createElement("input");
+    tempInput.value = result.innerText;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+    alert("Copied: " + result.innerText);
+  };
